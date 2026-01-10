dd3ec8ef5ca26acdbf1616335757fd1797c741ed
marinakreibring <timarina1997@yahoo.com>
2026-01-10 22:27:39 +0100

    add getsingle and getall functions to cse341

--- backend/controllers/professional.js
+++ backend/controllers/professional.js
@@ -2,20 +2,41 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
-  result.toArray().then((lists) => {
+  try {
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection('professional')
+      .find()
+      .toArray();
+
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const getSingle = async (req, res) => {
-  const contactId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('contacts').find({ _id: contactId });
-  result.toArray().then((lists) => {
+  try {
+    const contactId = new ObjectId(req.params.id);
+
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection('professional')
+      .findOne({ _id: contactId });
+
+    if (!result) {
+      res.status(404).json({ message: 'Contact not found' });
+      return;
+    }
+
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 module.exports = { getAll, getSingle };
