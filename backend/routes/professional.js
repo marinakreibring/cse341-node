2a448194b22f1201e5b0ab8c925db63fb56f1d2d
marinakreibring <timarina1997@yahoo.com>
2026-01-10 20:50:18 +0100

    1st commit to cse341

---
+++ backend/routes/professional.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const professionalController = require('../controllers/professional');
+
+const router = express.Router();
+
+// GET /feed/posts
+router.get('/', professionalController.getAll);
+router.get('/:id', professionalController.getSingle);
+// localhost:8080/professional/
+module.exports = router;
