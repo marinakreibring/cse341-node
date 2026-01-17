76883b4897d18ea94604a3d609ae1a0d36873c67
marinakreibring <timarina1997@yahoo.com>
2026-01-17 23:42:45 +0100

    polish app.js

--- backend/app.js
+++ backend/app.js
@@ -12,11 +12,13 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/', require('./routes'));
+
+
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
